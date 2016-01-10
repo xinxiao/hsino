f18e636d8d43ef9fcef8c90767d3ca8c3e5548f4
Xiao Xin <xin.xiao@hotmail.com>
2016-01-10 15:19:58 -0500

    Comments added

--- data/stock.py
+++ data/stock.py
@@ -1,9 +1,9 @@
-# ---------------------------------------- #
-# Investment product Information Collector	
+# ---------------------------------------------------------------------------- #
+# Investment Product Information Collector	
 # 
 # Author: Xiao Xin
 # Date: Dec. 20, 2015
-# ---------------------------------------- #
+# ---------------------------------------------------------------------------- #
 
 
 '''
@@ -26,7 +26,7 @@ INTERVAL = 60
 
 # Links to acquire information
 GOOGLE_FINANCE = 'https://www.google.com/finance'
-INFO = GOOGLE_FINANCE + '?q={}:{}' 
+INFO = GOOGLE_FINANCE + '?q={}:{}'
 PRICE = GOOGLE_FINANCE + '/getprices?x={}&q={}&i=60&p={}d&f=d,o,h,l,c,v'
 
 # Basic structure of .csv file
@@ -65,44 +65,48 @@ class Stock:
 	# Corresponding collection	
 	COLLECTION = DB['Stock']
 
+	# Constructor
 	def __init__(self, exchange, ticker):
+		# Save ticker number and exchange
 		self.exchange = exchange.upper()
 		self.ticker = ticker.upper()
 		
+		# Request more detailed information from Google Finance
 		req = requests.get(INFO.format(exchange, ticker)).text
 		info = BeautifulSoup(req, 'html.parser')
-		
+		meta = info.find_all('meta')		
+
+		# Extract information from google Finance 
 		self.company = str(
 				info.find_all(
 				'meta', 
 				{
 					'itemprop' : 'name'
 				}
-				)[0]['content'].replace('.', ''))
+				)[0]['content'].replace('.', ''))		# Find company name
 		self.location = str(info.find_all(
 				'meta',
                                 {
 					'itemprop' : 'exchangeTimezone'
 				}
-				)[0]['content'].replace('_', ' '))
+				)[0]['content'].replace('_', ' '))		# Find the location of the exchange
 		self.currency = str(info.find_all('meta', 
-			  	{'itemprop' : 'priceCurrency'})[0]['content'])
+			  	{'itemprop' : 'priceCurrency'}
+				)[0]['content'])				# Find out the pricing currency
 	
+	# Representation
 	def __repr__(self):
 		return format(self.info())
 
-	def is_stored(self):
+	# Check if the stock has been stored in local database
+	def has_stored(self):
 		cursor = COLLECTION.find({
                         	'exchange' : self.exchange,
                         	'ticker' : self.ticker
-                	 })
-
-		count = 0
-		for element in cursor:
-			count += 1
+                	 })							# Look for stock in database
 		
-		return count > 0
-		
+		return cursor.count() > 0					# Return true if stock were found	
+	# Return a dictionary of  the general information of the stock
 	def info(self):
 		stock = {
 				'exchange' : self.exchange,
@@ -110,79 +114,90 @@ class Stock:
 				'company' : self.company,
 				'location' : self.location,
 				'currency' : self.currency
-			}
+			}							# Put information in dictionary
 
 		return stock
 	
+	# Acquire the trading data of given recent days
+	#
+	# The update frequency of the data is set to be 1 update per 
+	# minute, for this value to be the highest frequency allowed by 
+	# Google Finance serveice
 	def detail(self, period):
+		# Request trading data
 		detail = str(requests.get(
 				PRICE.format(self.exchange,
 					     self.ticker, period)
 			  ).text).split('\n')
 		
+		# Processing the header of the request	
 		index = 1 
 		header = {}
 		offset = 0
-		while detail[index][0] != 'a':
+		while detail[index][0] != 'a':					# Loop to data
 			split = str(detail[index]).split('=')
-			if split[0] == 'TIMEZONE_OFFSET':
-				offset = int(split[1]) - LOCAL_TIME_OFFSET)
+			if split[0] == 'TIMEZONE_OFFSET':			# Look for timezone offset
+				offset = int(split[1]) - LOCAL_TIME_OFFSET
 				offset *= 60
 			index += 1
-		detail = detail[index:-1]
+		detail = detail[index:-1]					# Cut off header
 		
+		# Processing actual data
 		index = 0
 		stamp = 0
 		date = ''
 		result = {}
-		while index < len(detail):
-			split = detail[index].split(',')
-			head = split[0]
-			split = split[1:]
-
-			if head[0] == 'a':
-				head = int(head[1:]) + offset
-				stamp = head
+		while index < len(detail):					# Loop to end
+			split = detail[index].split(',')			# Split terms
+			head = split[0]						# Get the first term
+			split = split[1:]					# Save the rest
+			
+			# Check if a new date stamp appears	
+			if head[0] == 'a':					# If a new date stamp were found
+				head = int(head[1:]) + offset			# Parse date stamp; add time offset
+				stamp = head					# Reset date stamp
 				date = datetime.fromtimestamp(
-					stamp
-				      ).strftime('%Y-%m-%d')
-				result[date] = {}
-			else:
-				head = stamp + int(head) * INTERVAL
+						stamp
+					).strftime('%Y-%m-%d')			# Put date in proper format
+				result[date] = {}				# Set field in result for this dat
+			else:							# If no new date stamp were found
+				head = stamp + int(head) * INTERVAL		# Calculate exact timestamp
 
 			time = datetime.fromtimestamp(
 					head
-				).strftime('%H:%M')
+				).strftime('%H:%M')				# Calculate the time when data occurs
 			
-			for i in range(len(split) - 1):
+			for i in range(len(split) - 1):				# Parse each numeric value
 				split[i] = float(split[i])
 			split[-1] = int(split[-1])
 			
-			content = {}
+			content = {}						# Put data into term
 			for key in COLUMNS:
 				content[key] = split[COLUMNS[key]]
 
-			result[date][time] = content
+			result[date][time] = content				# Save the data for the time point 
 			index += 1
 
 		return result
 	
+	# Save the trading data of given recent days
 	def store(self, period):
-		if not self.is_stored():
-			info = self.info()
+		# Check if the stock had been previously stored in database
+		if not self.has_stored():					# If the stock has not been stored
+			info = self.info()					# Create stock general info header
 			info['detail'] = {}
-			COLLECTION.insert_one(info)
+			COLLECTION.insert_one(info)				# Insert stock info into database 
 
 		stock = COLLECTION.find({
 				'exchange' : self.exchange,
 				'ticker' : self.ticker
-			})[0]
+			})[0]							# Find the stored part of the stock
 		
 		detail = stock['detail']
-		update = self.detail(period)
+		update = self.detail(period)					# Acquire the data that needs storing
 
 		for key in update:
-			detail[key] = update[key]
+			detail[key] = update[key]				# Update the info of each day
 		
 		result = COLLECTION.update_one(
 			{
@@ -191,46 +206,57 @@ class Stock:
 
 			},
 			{ '$set' : { 'detail' : detail } },
-		)
+		)								# Put the data in database
 
-		return result.modified_count > 0	
-
+	# Export trading data of a given date to .csv file
+	#
+	# Return if the file has been generated 
 	def csv(self, date):
+		if not self.has_stored():
+			return False						# Return False if 
+										# the stock had not been stored
+
 		stock = COLLECTION.find({
                         	'exchange' : self.exchange,
                         	'ticker' : self.ticker
-                	})[0]
+                	})[0]							# Find the stock in database
+
 		if not date in stock['detail']:
-			return False
-                detail = stock['detail'][date]
+			return False						# Return False if the data of the 
+										# specific date is not in database
+                
+		detail = stock['detail'][date]					# Find the data of the date
 		
-		path = '~/Hsino/csv/{}/{}.csv'
+		path = '~/Hsino/csv/{}/{}.csv'					# Set the path of file
 		path = path.format(self.company, date)
 		
-		folder = os.path.dirname(path)
+		folder = os.path.dirname(path)					# Check if directories on path exists
 		if not os.path.exists(folder):
-			os.makedirs(folder)
+			os.makedirs(folder)					# Make directories if needed
 
-		access = 'a'
+		access = 'a'							# Set initial accessibility to append
 		if os.path.isfile(path):
-			access = 'w'
+			access = 'w'						# Update the file if files exists
 		
-		file = open(path, access)
+		file = open(path, access)					# Open file
 
-		file.write(HEAD)
+		file.write(HEAD)						# Write head of .csv file
 		
 		keys = detail.keys()
 		for i in range(len(keys)):
 			keys[i] = str(keys[i])
-		keys.sort()
+		keys.sort()							# Sort the trading data by time
 		
-		for time in keys:
-			file.write('\n')
-			line = range(len(COLUMNS))
-			info = detail[time]
+		for time in keys:						# Write data of every minute to file
+			file.write('\n')					# Switch line at the begining
+										# in case new line at very end
+			line = range(len(COLUMNS))				# Prepare to combine data components
+			info = detail[time]					# Get data at time point
 			for key in info:
-				line[COLUMNS[key]] = str(info[key])
-			line = [date, time] + line
-			file.write(','.join(line))
-		file.close()
-		return True
+				line[COLUMNS[key]] = str(info[key])		# Put each field in position
+			line = [date, time] + line				# Add date and time
+			file.write(','.join(line))				# Join the conponents by comma, 
+										# and write to file
+		file.close()							# Close file
+		return True							# Return True for file has been
+										# successfully generated
